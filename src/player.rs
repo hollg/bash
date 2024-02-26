ea004e2342feba81866eb72513e9060bae4024d5
Gary Holland <garyhollandxyz@gmail.com>
2024-02-26 22:28:35 +0000

    jump works, but is infinite

--- src/player.rs
+++ src/player.rs
@@ -8,11 +8,17 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_player)
             .add_plugins(InputManagerPlugin::<PlayerAction>::default())
-            .add_systems(Update, movement);
+            .add_systems(Update, (run, gravity, jump, rise));
     }
 }
 #[derive(Component)]
 struct Player;
+
+impl Player {
+    const DEFAULT_RUN_SPEED: f32 = 7.0;
+    const DEFAULT_JUMP_HEIGHT: f32 = 1.5;
+    const DEFAULT_JUMP_SPEED: f32 = 5.5;
+}
 
 #[derive(Bundle)]
 struct PlayerBundle {
@@ -54,14 +60,21 @@ fn spawn_player(
         },
         Collider::cuboid(0.5, 0.5, 0.5),
         RigidBody::KinematicPositionBased,
-        KinematicCharacterController {
-            translation: Some(Vec3::new(0.0, 15.0, 0.0)),
-            ..default()
-        },
+        KinematicCharacterController::default(),
     ));
 }
 
-fn movement(
+fn gravity(mut query: Query<&mut KinematicCharacterController, Without<Jump>>) {
+    for mut player in query.iter_mut() {
+        println!("gravity");
+        player.translation = match player.translation {
+            Some(vec) => Some(Vec3::new(vec.x, vec.y - 0.01, vec.z)),
+            None => Some(Vec3::new(0.0, -0.1, 0.0)),
+        };
+    }
+}
+
+fn run(
     mut query: Query<
         (
             &ActionState<PlayerAction>,
@@ -69,6 +82,7 @@ fn movement(
         ),
         With<Player>,
     >,
+    time: Res<Time>,
 ) {
     let (action_state, mut character_controller) = query.single_mut();
     let mut direction_vector = Vec3::ZERO;
@@ -76,23 +90,50 @@ fn movement(
     for input_direction in PlayerAction::DIRECTIONS {
         if action_state.pressed(&input_direction) {
             if let Some(direction) = input_direction.direction() {
-                println!("direction: {:?}", direction);
-                direction_vector.x += direction.x;
+                direction_vector.x +=
+                    direction.x * Player::DEFAULT_RUN_SPEED * time.delta_seconds();
             }
         }
     }
 
-    if action_state.just_pressed(&PlayerAction::Jump) {
-        println!("Jump");
-        direction_vector.y += 1.0;
-    }
+    character_controller.translation = match character_controller.translation {
+        Some(vec) => Some(Vec3::new(direction_vector.x, vec.y, vec.z)),
+        None => Some(direction_vector),
+    };
+}
 
-    //  gravity
-    direction_vector.y -= 0.1;
-    if let Ok(direction) = Direction3d::new(direction_vector) {
-        character_controller.translation = Some(*direction);
+fn jump(mut commands: Commands, mut query: Query<(&ActionState<PlayerAction>, Entity)>) {
+    let (action_state, entity) = query.single_mut();
+
+    if action_state.pressed(&PlayerAction::Jump) {
+        commands
+            .entity(entity)
+            .insert(Jump(Player::DEFAULT_JUMP_HEIGHT));
     }
 }
+
+fn rise(
+    mut commands: Commands,
+    mut query: Query<(&mut KinematicCharacterController, &Jump, &Transform, Entity)>,
+    time: Res<Time>,
+) {
+    if let Ok((mut controller, jump, transform, entity)) = query.get_single_mut() {
+        let upward_movement = Player::DEFAULT_JUMP_SPEED * time.delta_seconds();
+
+        if transform.translation.y + upward_movement >= jump.0 {
+            // remove jump component
+            commands.entity(entity).remove::<Jump>();
+        }
+        controller.translation = match controller.translation {
+            Some(vec) => Some(Vec3::new(vec.x, vec.y + upward_movement, vec.z)),
+            None => Some(Vec3::new(0.0, upward_movement, 0.0)),
+        }
+    };
+}
+
+#[derive(Component)]
+struct Jump(f32);
+
 #[derive(Actionlike, PartialEq, Eq, Hash, Clone, Copy, Debug, Reflect)]
 enum PlayerAction {
     Left,
